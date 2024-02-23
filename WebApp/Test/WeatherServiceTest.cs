65afa1f5a703ff4d65f6814f71f878362f3860db
duffscs <duffscs@gmail.com>
2024-02-23 15:39:56 +0100

    ajout de test

---
+++ WebApp/Test/WeatherServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using WebApp.Service;
+
+namespace WebApp.Test;
+
+
+public class WeatherServiceTest {
+
+	private HttpClient client;
+	private WeatherService service;
+
+
+	[SetUp]
+	public void Setup() {
+		client = new HttpClient { BaseAddress = new Uri("http://api.openweathermap.org/") };
+		service = new WeatherService(client);
+	}
+
+	[TearDown]
+	public void TearDown() {
+		client.Dispose();
+	}
+
+	[Test]
+	public async Task GetWeatherTest() {
+		var result = await service.GetWeather("35", "139");
+		Assert.That(result, Is.Not.Null);
+		Assert.That(result!.city.name, Is.EqualTo("Shuzenji"));
+	}
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+docker build -t mon-app-blazor-wasm .
+
+docker run -d -p 8080:80 mon-app-blazor-wasm
+
